cfd4cb10869cf4515c32192b380d9c120a711ca5
Kauã <knkauannewtonkn@gmail.com>
2024-07-27 23:32:54 -0400

    feat: Construção de layout; build: Adicionando o CSS do bootstrap 4.5;

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,2 @@
+<h1>está é uma pagina de contato!</h1>
+<a href="/">voltar para a home!</a>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <!-- Fonte do google -->
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">  
+
+        <!-- CSS do bootstrap -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        
+        <!-- CSS da aplicação -->
+        <link rel="stylesheet" href="/CSS/styles.css">
+        <script src="/js/scripts.js"></script>
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="collapse navbar-collapse" id="navbar">
+                    <a href="/" class="navbar-brand">
+                        <img src="/img/hdcevents_logo.svg" alt="HDC events">
+                    </a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Eventos</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Criar Eventos</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Entrar</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Cadastrar</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>    
+        @yield('content')
+        <footer>
+            <p>Eventos &copy; 2024</p>
+        </footer>
+    </body>
+</html>
